e6dd22b6380601895fc5ae979792fbd627290b68
S.P. Mohanty <spmohanty91@gmail.com>
2017-10-26 11:50:42 +0000

    Do default bind on 0.0.0.0

--- gym_http_server.py
+++ gym_http_server.py
@@ -662,7 +662,7 @@ def shutdown():
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Start a Gym HTTP API server')
-    parser.add_argument('-l', '--listen', help='interface to listen to', default='127.0.0.1')
+    parser.add_argument('-l', '--listen', help='interface to listen to', default='0.0.0.0')
     parser.add_argument('-p', '--port', default=5000, type=int, help='port to bind to')
 
     args = parser.parse_args()
